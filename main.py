9f523486f2bf54ba31329b553ced446f664311e0
unknown <bruno@brunoluna.com.br>
2024-07-31 17:54:44 -0300

    Versão 1

---
+++ main.py
@@ -0,0 +1,31 @@
+# criando variaveis, atribuindo valores a elas e imprimindo na tela.
+
+# criando as variaveis
+nome: str = 'Bruno Luna'
+cidade: str = 'Brasília'
+profissao: str = 'Programador'
+
+# imprimindo na tela variavéis strings
+print(nome)
+print(cidade)
+print(profissao)
+
+# criando variaveis, inteiro e atribuindo valores e imprimindo na tela.
+idade: int = 40
+dia_nascimento: int = 25
+ano_corrente: int = 2024
+
+# imprimindo na tela as variaveis inteiras.
+print(idade)
+print(dia_nascimento)
+print(ano_corrente)
+
+# criando variaveis, float e atribuindo valores e imprimindo na tela.
+altura: float = 1.85
+peso: float = 100.1
+nota_curso: float = 99.9
+
+# imprimindo na tela as variaveis float.
+print(altura)
+print(peso)
+print(nota_curso)
